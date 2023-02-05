ece9dea17205e58f522487c5d9ff9c04cef69cdf
ersan <ersanmaz@gmail.com>
2023-02-05 21:38:19 +0100

    Add k8s details and modify client

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,33 +1,30 @@
 let ws = null;
 let url = "ws://localhost:8080/echo/uppercase";
 
-function setConnected(connected)
-{
+function setConnected(connected) {
     document.getElementById('connect').disabled = connected;
     document.getElementById('disconnect').disabled = !connected;
     document.getElementById('echo').disabled = !connected;
 }
 
-function connect()
-{
+function connect() {
     ws = new WebSocket(url);
-    ws.onopen = function() {
+    ws.onopen = function () {
         setConnected(true);
         log('Info: Connection Established.');
     };
 
-    ws.onmessage = function(event) {
-        log(event.data);
+    ws.onmessage = function (event) {
+        log('Response from server:: ' + event.data);
     };
 
-    ws.onclose = function(event) {
+    ws.onclose = function (event) {
         setConnected(false);
         log('Info: Closing Connection.');
     };
 }
 
-function disconnect()
-{
+function disconnect() {
     if (ws != null) {
         ws.close();
         ws = null;
@@ -35,22 +32,19 @@ function disconnect()
     setConnected(false);
 }
 
-function echo()
-{
-    if (ws != null)
-    {
-        var message = document.getElementById('message').value;
-        log('Sent to server :: ' + message);
+function echo() {
+    if (ws != null) {
+        let message = document.getElementById('message').value;
+        log('Sent to server:: ' + message);
         ws.send(message);
     } else {
         alert('connection not established, please connect.');
     }
 }
 
-function log(message)
-{
-    var console = document.getElementById('logging');
-    var p = document.createElement('p');
+function log(message) {
+    let console = document.getElementById('logging');
+    let p = document.createElement('p');
     p.appendChild(document.createTextNode(message));
     console.appendChild(p);
 }
